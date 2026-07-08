5c00e403d69435980f5f0a1b3b0350f6e105db79
Rosuelo996 <rosuelo996.sr@gmail.com>
2026-07-08 11:13:21 +0100

    fix carousel skills spacing

--- src/app/components/ui/SkillsCarousel.tsx
+++ src/app/components/ui/SkillsCarousel.tsx
@@ -2,53 +2,52 @@ import useEmblaCarousel from "embla-carousel-react";
 import AutoScroll from "embla-carousel-auto-scroll";
 
 const SkillsCarousel = () => {
+  const skills = [
+    "React",
+    "Next.js",
+    "TypeScript",
+    "JavaScript",
+    "Redux Toolkit",
+    "Tailwind CSS",
+    "GSAP",
+    "REST APIs",
+    "Stripe",
+    "Firebase",
+    "Vercel",
+    "Git",
+  ];
 
-const skills = [
-  "React",
-  "Next.js",
-  "TypeScript",
-  "JavaScript",
-  "Redux Toolkit",
-  "Tailwind CSS",
-  "GSAP",
-  "REST APIs",
-  "Stripe",
-  "Firebase",
-  "Vercel",
-  "Git"
-];
-
-const [emblaRef] = useEmblaCarousel(
-  {
-    loop: true,
-    dragFree: false,
-    watchDrag: false,
-  },
-  [
-    AutoScroll({
-      playOnInit: true,
-      speed: 1,
-      startDelay: 0,
-      stopOnInteraction: false,
-      stopOnMouseEnter: false,
-      stopOnFocusIn: false,
-    }),
-  ]
-);
+  const [emblaRef] = useEmblaCarousel(
+    {
+      loop: true,
+      dragFree: true,
+      watchDrag: true,
+    },
+    [
+      AutoScroll({
+        playOnInit: true,
+        speed: 1,
+        startDelay: 0,
+        stopOnInteraction: false,
+        stopOnMouseEnter: false,
+        stopOnFocusIn: false,
+      }),
+    ],
+  );
 
   return (
     <section id="skills">
-   <div className="skills__viewport" ref={emblaRef}>
-  <div className="skills__track">
-    {skills.map((skill) => (
-      <div className="skills__slide" key={skill}>
-        <span className="skills__separator">✦</span>
-        <p className="skills__name">{skill}</p>
+      <div className="skills__viewport" ref={emblaRef}>
+        <div className="skills__track">
+          {skills.map((skill) => (
+            <div className="skills__slide" key={skill}>
+              <span className="skills__separator">✦</span>
+              <p className="skills__name">{skill}</p>
+            </div>
+          ))}
+        </div>
       </div>
-    ))}
-  </div>
-  </div>
-</section>
+    </section>
   );
 };
 
