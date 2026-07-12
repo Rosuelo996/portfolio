8c8d955218209250a45b2fe033aa2408699084fd
Rosuelo996 <rosuelo996.sr@gmail.com>
2026-07-12 14:46:37 +0100

    fix aspect ratio >1200px

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   allowCypressEnv: false,
 
   e2e: {
-    baseUrl: "http://localhost:3001",
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
