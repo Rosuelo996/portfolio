4a6e8a1f1363c0eccdba48807730d83074925e35
Rosuelo996 <rosuelo996.sr@gmail.com>
2026-08-26 19:03:58 +0100

    fix img

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -50,7 +50,7 @@ const Projects = () => {
                 >
                   <Image
                     className="project__image"
-                    src="/icons/shopfloor2.png"
+                    src="/icons/shopfloor.png"
                     alt="ShopFloor store management dashboard"
                     width={1600}
                     height={900}
